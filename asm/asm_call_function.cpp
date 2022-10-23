b2ceb89ce7fd1f8835193e9d7c893b82c7e17789
Todor Arnaudov <twenkid@gmail.com>
2022-10-23 05:12:59 +0300

    Create asm_call_function.cpp

---
+++ asm/asm_call_function.cpp
@@ -0,0 +1,24 @@
+// InlineAssembler_Calling_C_Functions_in_Inline_Assembly.cpp
+//VS 2003 Retired documentation
+#include <stdio.h>
+char format[] = "%s %s\n";
+char hello[] = "Hello";
+char world[] = "world";
+int main( void )
+{
+__asm
+	{
+	mov eax, offset world
+	push eax
+	mov eax, offset hello
+	push eax
+	mov eax, offset format
+	push eax
+	call printf
+	//clean up the stack so that main can exit cleanly
+	//use the unused register ebx to do the cleanup
+	pop ebx
+	pop ebx
+	pop ebx
+	}
+}
